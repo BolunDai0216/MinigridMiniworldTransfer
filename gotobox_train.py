0d1dd8f5a60c574dfd5f06746a2f9be721a80d2e
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2023-04-24 10:44:26 -0400

    added gotobox env

---
+++ gotobox_train.py
@@ -0,0 +1,175 @@
+import argparse
+from datetime import datetime
+from pdb import set_trace
+from time import time
+
+import gymnasium as gym
+import minigrid
+import numpy as np
+import torch as th
+import torch.nn as nn
+from gymnasium.core import ObservationWrapper
+from gymnasium.spaces import Box, Dict
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import CheckpointCallback
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+
+from minigrid_gotobox import GoToBoxEnv
+
+
+class BoxObsWrapper(ObservationWrapper):
+    def __init__(self, env):
+        """A wrapper that makes image the only observation.
+        Args:
+            env: The environment to apply the wrapper
+        """
+        super().__init__(env)
+
+        self.observation_space = Dict(
+            {
+                "image": env.observation_space.spaces["image"],
+                "box_color": Box(low=0.0, high=1.0, shape=(6,), dtype=np.float32),
+            }
+        )
+
+        self.color_one_hot_dict = {
+            "red": np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
+            "green": np.array([0.0, 1.0, 0.0, 0.0, 0.0, 0.0]),
+            "blue": np.array([0.0, 0.0, 1.0, 0.0, 0.0, 0.0]),
+            "purple": np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0]),
+            "yellow": np.array([0.0, 0.0, 0.0, 0.0, 1.0, 0.0]),
+            "grey": np.array([0.0, 0.0, 0.0, 0.0, 0.0, 1.0]),
+        }
+
+    def observation(self, obs):
+        wrapped_obs = {
+            "image": obs["image"],
+            "box_color": self.color_one_hot_dict[self.target_color],
+        }
+
+        return wrapped_obs
+
+
+class BoxEnvExtractor(BaseFeaturesExtractor):
+    def __init__(self, observation_space: Dict):
+        # We do not know features-dim here before going over all the items,
+        # so put something dummy for now. PyTorch requires calling
+        # nn.Module.__init__ before adding modules
+        super().__init__(observation_space, features_dim=1)
+
+        extractors = {}
+        total_concat_size = 0
+
+        # We need to know size of the output of this extractor,
+        # so go over all the spaces and compute output feature sizes
+        for key, subspace in observation_space.spaces.items():
+            if key == "image":
+                # We will just downsample one channel of the image by 4x4 and flatten.
+                # Assume the image is single-channel (subspace.shape[0] == 0)
+                cnn = nn.Sequential(
+                    nn.Conv2d(3, 16, (2, 2)),
+                    nn.ReLU(),
+                    nn.Conv2d(16, 32, (2, 2)),
+                    nn.ReLU(),
+                    nn.Conv2d(32, 64, (2, 2)),
+                    nn.ReLU(),
+                    nn.Flatten(),
+                )
+
+                # Compute shape by doing one forward pass
+                with th.no_grad():
+                    n_flatten = cnn(
+                        th.as_tensor(subspace.sample()[None]).float()
+                    ).shape[1]
+
+                linear = nn.Sequential(nn.Linear(n_flatten, 64), nn.ReLU())
+                extractors["image"] = nn.Sequential(*(list(cnn) + list(linear)))
+                total_concat_size += 64
+
+            elif key == "box_color":
+                extractors["box_color"] = nn.Linear(subspace.shape[0], 32)
+                total_concat_size += 32
+
+        self.extractors = nn.ModuleDict(extractors)
+
+        # Update the features dim manually
+        self._features_dim = total_concat_size
+
+    def forward(self, observations) -> th.Tensor:
+        encoded_tensor_list = []
+
+        # self.extractors contain nn.Modules that do all the processing.
+        for key, extractor in self.extractors.items():
+            encoded_tensor_list.append(extractor(observations[key]))
+
+        # Return a (B, self._features_dim) PyTorch tensor, where B is batch dimension.
+        return th.cat(encoded_tensor_list, dim=1)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", action="store_true", help="train the model")
+    parser.add_argument("--load_model", default="minigrid_door_20230418-174509")
+    parser.add_argument("--render", action="store_true", help="render trained models")
+    args = parser.parse_args()
+
+    policy_kwargs = dict(features_extractor_class=BoxEnvExtractor)
+
+    # Create time stamp of experiment
+    stamp = datetime.fromtimestamp(time()).strftime("%Y%m%d-%H%M%S")
+
+    if args.train:
+        env = GoToBoxEnv()
+        env = BoxObsWrapper(env)
+
+        checkpoint_callback = CheckpointCallback(
+            save_freq=1e5,
+            save_path=f"./models/ppo/minigrid_gotobox_{stamp}/",
+            name_prefix="iter",
+        )
+
+        model = PPO(
+            "MultiInputPolicy",
+            env,
+            policy_kwargs=policy_kwargs,
+            verbose=1,
+            tensorboard_log="./logs/ppo/minigrid_gotobox_tensorboard/",
+        )
+        model.learn(
+            2e6,
+            tb_log_name=f"{stamp}",
+            callback=checkpoint_callback,
+        )
+    else:
+        if args.render:
+            env = GoToBoxEnv(render_mode="human")
+        else:
+            env = GoToBoxEnv()
+        env = BoxObsWrapper(env)
+
+    ppo = PPO("MultiInputPolicy", env, policy_kwargs=policy_kwargs, verbose=1)
+
+    # add the experiment time stamp
+    ppo = ppo.load(f"models/ppo/{args.load_model}", env=env)
+
+    obs, info = env.reset()
+    rewards = 0
+
+    for i in range(2000):
+        action, _state = ppo.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, info = env.step(action)
+        rewards += reward
+
+        if terminated or truncated:
+            print(f"Test reward: {rewards}")
+            obs, info = env.reset()
+            rewards = 0
+            continue
+
+    print(f"Test reward: {rewards}")
+
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ minigrid_gotobox.py
@@ -0,0 +1,116 @@
+from __future__ import annotations
+
+from minigrid.core.constants import COLOR_NAMES
+from minigrid.core.grid import Grid
+from minigrid.core.mission import MissionSpace
+from minigrid.core.world_object import Box
+from minigrid.manual_control import ManualControl
+from minigrid.minigrid_env import MiniGridEnv
+
+
+class GoToBoxEnv(MiniGridEnv):
+    def __init__(
+        self,
+        size=11,
+        agent_start_pos=(5, 5),
+        agent_start_dir=0,
+        max_steps: int | None = None,
+        **kwargs,
+    ):
+        self.agent_start_pos = agent_start_pos
+        self.agent_start_dir = agent_start_dir
+
+        self.size = size
+        mission_space = MissionSpace(
+            mission_func=self._gen_mission,
+            ordered_placeholders=[COLOR_NAMES],
+        )
+
+        if max_steps is None:
+            max_steps = 4 * size**2
+
+        super().__init__(
+            mission_space=mission_space,
+            width=size,
+            height=size,
+            # Set this to True for maximum speed
+            see_through_walls=True,
+            max_steps=max_steps,
+            **kwargs,
+        )
+
+    @staticmethod
+    def _gen_mission(color: str):
+        return f"go to the {color} box"
+
+    def _gen_grid(self, width, height):
+        # Create the grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, width, height)
+
+        # Generate the 4 doors at random positions
+        boxPos = []
+        boxPos.append((5, 1))
+        boxPos.append((1, 5))
+        boxPos.append((9, 5))
+        boxPos.append((5, 9))
+
+        # Generate the door colors
+        boxColors = []
+        while len(boxColors) < len(boxPos):
+            color = self._rand_elem(COLOR_NAMES)
+            if color in boxColors:
+                continue
+            boxColors.append(color)
+
+        # Place the doors in the grid
+        for idx, pos in enumerate(boxPos):
+            color = boxColors[idx]
+            self.grid.set(*pos, Box(color))
+
+        # Place the agent
+        if self.agent_start_pos is not None:
+            self.agent_pos = self.agent_start_pos
+            self.agent_dir = self.agent_start_dir
+        else:
+            self.place_agent()
+
+        # Select a random target door
+        boxIdx = self._rand_int(0, len(boxPos))
+        self.target_pos = boxPos[boxIdx]
+        self.target_color = boxColors[boxIdx]
+
+        # Generate the mission string
+        self.mission = f"go to the {self.target_color} box"
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info = super().step(action)
+
+        ax, ay = self.agent_pos
+        tx, ty = self.target_pos
+
+        # Don't let the agent open any of the doors
+        if action == self.actions.toggle:
+            terminated = True
+
+        # Reward performing done action in front of the target door
+        if action == self.actions.done:
+            if (ax == tx and abs(ay - ty) == 1) or (ay == ty and abs(ax - tx) == 1):
+                reward = self._reward()
+            terminated = True
+
+        return obs, reward, terminated, truncated, info
+
+
+def main():
+    env = GoToBoxEnv(render_mode="human")
+
+    # enable manual control for testing
+    manual_control = ManualControl(env, seed=42)
+    manual_control.start()
+
+
+if __name__ == "__main__":
+    main()
