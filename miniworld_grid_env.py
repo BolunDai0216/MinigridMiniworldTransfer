ff0328610c69148ffc14a4cfbf61a8bf3632c62e
Bolun <36321182+BolunDai0216@users.noreply.github.com>
2023-04-30 17:30:36 -0400

    updated miniworld_grid_env

--- miniworld_grid_env.py
+++ miniworld_grid_env.py
@@ -1,13 +1,15 @@
+from pdb import set_trace
+
 import numpy as np
 from gymnasium import spaces, utils
-from miniworld.entity import Box
+from miniworld.entity import COLOR_NAMES, Box
 from miniworld.manual_control import ManualControl
 from miniworld.miniworld import MiniWorldEnv
 from miniworld.params import DEFAULT_PARAMS
 
 
 class MiniworldGridEnv(MiniWorldEnv, utils.EzPickle):
-    def __init__(self, size=10, max_episode_steps=180, **kwargs):
+    def __init__(self, size=9, max_episode_steps=100, **kwargs):
         assert size >= 2
         self.size = size
 
@@ -29,16 +31,45 @@ class MiniworldGridEnv(MiniWorldEnv, utils.EzPickle):
     def _gen_world(self):
         self.add_rect_room(min_x=0, max_x=self.size, min_z=0, max_z=self.size)
 
-        self.box = self.place_entity(
-            Box(color="red"), pos=np.array([1.5, 0.5, 5.5]), dir=0.0
+        colors = np.random.choice(COLOR_NAMES, size=4, replace=False)
+
+        self.box_left = self.place_entity(
+            Box(color=colors[0]), pos=np.array([0.5, 0.5, 4.5]), dir=0.0
         )
-        self.place_agent(pos=np.array([1.5, 0.5, 1.5]), dir=0.0)
+        self.box_bottom = self.place_entity(
+            Box(color=colors[1]), pos=np.array([4.5, 0.5, 8.5]), dir=0.0
+        )
+        self.box_right = self.place_entity(
+            Box(color=colors[2]), pos=np.array([8.5, 0.5, 4.5]), dir=0.0
+        )
+        self.box_up = self.place_entity(
+            Box(color=colors[3]), pos=np.array([4.5, 0.5, 0.5]), dir=0.0
+        )
+
+        self.boxes = [self.box_left, self.box_bottom, self.box_right, self.box_up]
+
+        # Select a random target door
+        boxIdx = np.random.choice(4, size=1)[0]
+        self.target_box = self.boxes[boxIdx]
+        self.target_color = colors[boxIdx]
+
+        # Generate the mission string
+        self.mission = f"go to the {self.target_color} box"
+        print(self.mission)
+
+        self.place_agent(pos=np.array([4.5, 0.5, 4.5]), dir=0.0)
 
     def step(self, action):
         obs, reward, termination, truncation, info = super().step(action)
 
-        if self.near(self.box):
-            reward += self._reward()
+        ax, ay = self.agent.pos[0], self.agent.pos[2]
+        tx, ty = self.target_box.pos[0], self.target_box.pos[2]
+
+        if action == self.actions.done:
+            if (ax == tx and int(abs(ay - ty)) == 1) or (
+                ay == ty and int(abs(ax - tx)) == 1
+            ):
+                reward += self._reward()
             termination = True
 
         return obs, reward, termination, truncation, info
